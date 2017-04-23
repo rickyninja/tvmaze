12bfb13ede3f66dd1dfe6e24f94867280f341dbb
Jeremy Singletary <jeremys@rickyninja.net>
2017-04-22 21:45:14 -0700

    Add godoc

--- tvmaze.go
+++ tvmaze.go
@@ -15,6 +15,7 @@ import (
 	cache "github.com/robfig/go-cache"
 )
 
+// Client is a tvmaze client.
 type Client struct {
 	Debug     bool
 	BaseURI   string
@@ -25,6 +26,7 @@ type Client struct {
 	*http.Client
 }
 
+// NewClient returns a ready to use Client.
 func NewClient(cachefile string) (*Client, error) {
 	c := cache.New(time.Minute*60*24*7, time.Minute*60)
 	if _, err := os.Stat(cachefile); err == nil {
@@ -47,6 +49,7 @@ func NewClient(cachefile string) (*Client, error) {
 	}, nil
 }
 
+// WriteCache writes cache contents to disk.
 func (c *Client) WriteCache() error {
 	err := c.Cache.SaveFile(c.CacheFile)
 	if err != nil {
@@ -55,6 +58,7 @@ func (c *Client) WriteCache() error {
 	return nil
 }
 
+// FindShow searches tvmaze for showname, and returns it as a Show if a match is found.
 func (c *Client) FindShow(showname string) (Show, error) {
 	candidates, err := c.GetShow(showname)
 	if err != nil {
@@ -83,6 +87,7 @@ func (c *Client) FindShow(showname string) (Show, error) {
 	return Show{}, errors.New("Failed to match show in tvmaze!")
 }
 
+// GetShow queries tvmaze for show, and returns Candidates that may be a match.
 func (c *Client) GetShow(show string) ([]Candidate, error) {
 	route := "/search/shows"
 	uri, err := url.Parse(c.BaseURI + route)
@@ -108,6 +113,7 @@ func (c *Client) GetShow(show string) ([]Candidate, error) {
 	return candidates, nil
 }
 
+// GetEpisodes queries tvmaze, and returns a list of Episodes.
 func (c *Client) GetEpisodes(showID int64) ([]Episode, error) {
 	route := fmt.Sprintf("/shows/%d/episodes", showID)
 	uri, err := url.Parse(c.BaseURI + route)
@@ -129,6 +135,8 @@ func (c *Client) GetEpisodes(showID int64) ([]Episode, error) {
 	return episodes, nil
 }
 
+// Go does an HTTP GET to tvmaze with the provided uri, and returns the response body.
+// It will cache response if UseCache is true.
 func (c *Client) Go(uri *url.URL) ([]byte, error) {
 	data, found := c.Cache.Get(uri.String())
 
@@ -164,6 +172,7 @@ func (c *Client) Go(uri *url.URL) ([]byte, error) {
 	return data.([]byte), nil
 }
 
+// Episode represents a tv episode.
 type Episode struct {
 	ID       int64
 	URL      string
@@ -179,11 +188,13 @@ type Episode struct {
 	Links   Links `json:"_links"`
 }
 
+// Candidate represents a search candidate.
 type Candidate struct {
 	Score float64
 	Show  Show
 }
 
+// Show represents a tv show.
 type Show struct {
 	ID        int64
 	URL       string
@@ -206,40 +217,48 @@ type Show struct {
 	Links     Links
 }
 
+// Links represents Episode links.
 type Links struct {
 	Self            Link
 	PreviousEpisode Link
 }
 
+// Link represents a uri link.
 type Link struct {
 	Href string
 }
 
+// Image represents an image.
 type Image struct {
 	Medium   string
 	Original string
 }
 
+// External represents a 3rd party tv api.
 type External struct {
 	TVRage  int64
 	TheTVDB int64
 }
 
+// Schedule represents a Show schedule.
 type Schedule struct {
 	Time string
 	Days []string
 }
 
+// Rating represents a tv show rating.
 type Rating struct {
 	Average float64
 }
 
+// Network represents the tv network airing the show.
 type Network struct {
 	ID      int
 	Name    string
 	Country Country
 }
 
+// Country represents the country the show aired in.
 type Country struct {
 	Name     string
 	Code     string
